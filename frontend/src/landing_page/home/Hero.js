09a1e8ed96180ef86824cf6ff2832cd7988a57bd
saifulRehaman-16 <24211a05bh@bvrit.ac.in>
2026-07-02 23:35:14 +0530

    Fixed Err

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <div className='container p-5 mb-5'>
         <div className='row text-center'>
-            <img src='media/images/Homehero.png' alt='Hero image' className='mb-5' />
+            <img src='media/images/homeHero.png' alt='Hero image' className='mb-5' />
             <h1 className="mt-5">Invest in Every Thing</h1>
             <p> Online Platform to invest in stocks, derivates, Mutual Funds and more</p>
             <button className='p-2 rounded-2 btn btn-primary fs-5 mb-5' style={{width:"20%", margin:"0 auto", }}>Signup Now</button>
